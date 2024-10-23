1ff42cc936fd481de30545392db42a1d0090a543
mufaqar <mufaqar@gmail.com>
2024-10-23 18:24:11 +0500

    child theme

--- template-parts/custom-sidebar.php
+++ template-parts/custom-sidebar.php
@@ -1,6 +1,6 @@
 <div class="sform_title_div">
             <h3 class="sform_title">
-                Make Your First Step Towards Canada
+                Make Your First Step Towards Canada 123
             </h3>
         </div>
 <?php echo do_shortcode('[contact-form-7 id="4390e49" title="Services Form"]');?>

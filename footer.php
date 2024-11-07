d593afa05433aba9b21d0ba7546e7a9e8c3bfb2b
mufaqar <mufaqar@gmail.com>
2024-11-07 17:51:32 +0500

    Footer

---
+++ footer.php
@@ -0,0 +1,42 @@
+		<?php 
+		/**
+		 * Default template location: immiex/footer
+		 *
+		 * @hooked immiex_newsletter_form_template_part - 10
+		 */
+		do_action( 'immiex/footer/before' ); 
+		?>
+		
+		<footer id="<?php immiex_footer_id(); ?>" <?php immiex_footer_class(); ?>>
+			<div class="container">
+
+				<?php 
+				/**
+				 * Default template location: immiex/footer
+				 *
+				 * @hooked immiex_footer_widget_area_template_part - 10
+				 * @hooked immiex_footer_copyright_template_part - 20
+				 */
+				do_action( 'immiex/footer' ); 
+				?>				
+				
+
+			</div> <!-- End .container -->	
+			<div class="parallax-inner"></div>									
+		</footer> <!-- End footer -->
+
+		<?php 
+		/**
+		 * Default template location: immiex/footer
+		 *
+		 * @hooked immiex_quick_contact_form_template_part - 10
+		 */
+		do_action( 'immiex/footer/after' ); 
+		?>
+		
+	</div>	<!-- End #<?php immiex_wrapper_id(); ?> (Start in header.php) -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

341bd50bcaa3722459f557312c94e82c2cdfd69b
mufaqar <mufaqar@gmail.com>
2024-11-07 19:08:24 +0500

    CSS

--- functions.php
+++ functions.php
@@ -4,12 +4,11 @@ function immiex_child_enqueue_styles() {
     $parent_style = 'immiex-style';
     $dependency = array('bootstrap', 'immiex-default-style');   
 
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css', $dependency );
+    wp_enqueue_style( 'immiex-child-style', get_stylesheet_directory_uri() . '/style.css',  array(  )   );   
 	wp_enqueue_style( 'Inter-google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap', false );
-   
+
+
+    
 }
 add_action( 'wp_enqueue_scripts', 'immiex_child_enqueue_styles' );
-
-function add_custom_css_link() {
-    echo '<link rel="stylesheet" href="' . get_stylesheet_directory_uri() . '/custom.css">';
-}
-add_action('wp_head', 'add_custom_css_link', 100);

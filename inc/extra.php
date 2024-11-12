cea2173562dc6983e3ffe22a9cf4079a422f1c40
mufaqar <mufaqar@gmail.com>
2024-11-12 18:24:34 +0500

    BD

--- inc/extra.php
+++ inc/extra.php
@@ -1,4 +1,48 @@
 <?php
+
+function cptui_register_my_cpts() {
+
+	/**
+	 * Post Type: Business Listings.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Business Listings", "immiex" ),
+		"singular_name" => esc_html__( "Business Listing", "immiex" ),
+	];
+
+	$args = [
+		"label" => esc_html__( "Business Listings", "immiex" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => true,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "business-listings", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+		"show_in_graphql" => false,
+	];
+
+	register_post_type( "business-listings", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts' );
+
+
 function cptui_register_my_taxes() {
 
 	/**
@@ -101,6 +145,8 @@ function cptui_register_my_taxes() {
 	register_taxonomy( "business_type", [ "business-listings" ], $args );
 }
 add_action( 'init', 'cptui_register_my_taxes' );
+
+
 
 if( function_exists('acf_add_local_field_group') ):
 
